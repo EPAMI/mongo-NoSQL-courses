038f02222bd369ef1a3ea339dfb4692c641de883
EPAMI <90223377+EPAMI@users.noreply.github.com>
2023-11-28 10:15:43 +0100

    refactor feature tags

---
+++ src/controllers/courseController.js
@@ -0,0 +1,64 @@
+const Course = require("../models/courseModel");
+
+const alllCourses = async (req, res) => {
+  try {
+    let courses = await Course.find();
+    res.json(courses);
+  } catch (error) {
+    res.json({ message: error.message });
+  }
+};
+const postCourse = async (req, res) => {
+  const { name, price, isPublish, tags } = req.body;
+  try {
+    
+    let newCourse = await new Course({ name, price, isPublish });
+    newCourse.tags = tags ? tags.split(","): []
+    newCourse.save();
+    res.json(newCourse);
+  } catch (error) {
+    res.json({ message: error.message });
+  }
+};
+const getCourse = async (req, res) => {
+  try {
+    let course = await Course.findOne({ _id: req.params.id });
+    res.json(course);
+  } catch (error) {
+    res.json({ message: error.message });
+  }
+};
+const putCourse = async (req, res) => {
+  const { name, price, isPublish, tags } = req.body;
+  try {
+    const update = {
+      name:name, 
+      price:price,
+      isPublish:isPublish,
+      tags:tags ? tags.split(","): []
+    }
+    let course = await Course.findOneAndUpdate(
+      { _id: req.params.id },
+      update,{new:true}
+    );
+    res.json({ message: "Course updated successfully", course });
+  } catch (error) {
+    res.json({ message: error.message });
+  }
+};
+const removeCourse = async (req, res) => {
+  try {
+    let course = await Course.findOneAndDelete({ _id: req.params.id });
+    res.json({ message: "Course deleted successfully", deletedCourse: course });
+  } catch (error) {
+    res.json({ message: error.message });
+  }
+};
+
+module.exports = {
+  alllCourses,
+  postCourse,
+  getCourse,
+  putCourse,
+  removeCourse,
+};
